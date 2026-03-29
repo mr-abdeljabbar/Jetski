0fd760f86cce98f4580f17fca96e93bcefba91dc
mr-abdeljabbar <mr.abdeljabbarbougrine@gmail.com>
2026-03-29 22:54:14 +0100

    chore: remove temporary debug scripts

--- check_bookings.mjs
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const p = new PrismaClient();
-const r = await p.booking.findMany({
-  select: { id: true, activityId: true, startDate: true, endDate: true, status: true }
-});
-console.log(JSON.stringify(r, null, 2));
-await p.$disconnect();
